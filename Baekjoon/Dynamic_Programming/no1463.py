c22614fa495ffc121696bdc4c2f315eeba099db5
woon1 <woon1998@naver.com>
2023-02-16 00:05:03 +0900

    update Baekjoon d230216

--- Baekjoon/Dynamic_Programming/no1463.py
+++ Baekjoon/Dynamic_Programming/no1463.py
@@ -1,14 +1,18 @@
 # 1로 만들기
 
 x = int(input())
+count = 0
+
+x = x - 1
+count = count + 1
 
 while x != 1:
     if x % 3 == 0:
         x = x // 3
     elif x % 2 == 0:
         x = x // 2
-    else:
-        x = x - 1
     count = count + 1
+    print(x)
+
 
 print(count)
